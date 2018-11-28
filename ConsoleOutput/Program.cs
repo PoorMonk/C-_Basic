8a5efe2e4d032592eb962dd5cc2c2cd9c21ab589
贺济 <ji.he@3glasses.com>
2018-11-28 11:59:08 +0800

    consoleOutput

---
+++ ConsoleOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.Write("One");
+            Console.Write("Two");
+            Console.Write("Three");
+
+            Console.WriteLine("Four");
+            Console.WriteLine("Five");
+            Console.WriteLine("Six");
+
+            Console.WriteLine("two numbers add: {0} + {1} = {2}", 1, 2, 3);
+        }
+    }
+}
