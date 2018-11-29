50ccffaa12609000117ad990b86000cffc0e6973
贺济 <ji.he@3glasses.com>
2018-11-29 17:43:52 +0800

    Test Console.Read

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -6,11 +6,13 @@ namespace InputOutput
     {
         static void Main(string[] args)
         {
-            string strNum1 = Console.ReadLine();
-            string strNum2 = Console.ReadLine();
-            int iNum1 = Convert.ToInt32(strNum1);
-            int iNum2 = Convert.ToInt32(strNum2);
-            Console.WriteLine("{0} + {1} = {2}.", iNum1, iNum2, iNum1 + iNum2);
+            // string strNum1 = Console.ReadLine();
+            // string strNum2 = Console.ReadLine();
+            // int iNum1 = Convert.ToInt32(strNum1);
+            // int iNum2 = Convert.ToInt32(strNum2);
+            // Console.WriteLine("{0} + {1} = {2}.", iNum1, iNum2, iNum1 + iNum2);
+            int i = Console.Read(); // 1 => 49
+            Console.WriteLine(Convert.ToInt32(i));
         }
     }
 }
